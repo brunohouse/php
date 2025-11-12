5bdec8fc727deef954445bac1e57415699e22c2d
Bruno Pupo <brunopupo1@gmail.com>
2025-11-12 11:47:16 -0300

    agora acho que foi

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 
-<<<<<<< HEAD
   echo "Ola mundo!123456" 
   echo "outra coisa outro echo testando conflitos"
-=======
-  echo "Ola mundo!" 
-  echo "Testando conflitos"
->>>>>>> fcc50c63ce1b7fb5554da3ebf782b090e9ee06cc
+  echo "conflitos resolvidos"
 
 ?>
